a70cf74246fa62ff86f536e5ce3d2cce5b7255f1
Chris Karpyszyn <chris.karpyszyn@gmail.com>
2025-11-04 12:50:47 -0500

    Create landing page and reorganize routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import Calendar from './Calendar';
+import Landing from './pages/Landing';
 import Admin from './pages/Admin';
 import Survey from './pages/Survey';
 import TwentyFourHourSchedule from './TwentyFourHourSchedule';
@@ -10,7 +11,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Calendar />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/calendar" element={<Calendar />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/survey" element={<Survey />} />
         <Route path="/24hour-schedule" element={<TwentyFourHourSchedule />} />
